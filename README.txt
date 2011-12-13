8ab31eabb26f74de96387310ef8cb1abf08e1154
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-12-13 21:48:44 +0000

    New project

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+The Flickr Portlet synposizes Flickr content related to places via Pleiades
+machine tags.
+
---
+++ pleiades/portlet/flickr/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+FlickrPortletMessageFactory = MessageFactory('pleiades.portlet.flickr')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
