646d0acc1e72f29bec281d151c1ed133aabdb698
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-12-15 18:27:55 +0000

    Sort portrait candidates by views, taking most viewed for the portrait.

--- pleiades/portlet/flickr/browser.py
+++ pleiades/portlet/flickr/browser.py
@@ -14,12 +14,21 @@ log = logging.getLogger("pleiades.portlet.flickr")
 FLICKR_API_ENDPOINT = "http://api.flickr.com/services/rest/"
 FLICKR_API_KEY = "b5899a9f7069f933b46b32b949ab4afd"
 FLICKR_TAGS_BASE = "http://www.flickr.com/photos/tags/"
+PLEIADES_PLACES_ID = "1876758@N22"
+IMG_TMPL = "http://farm%(farm)s.staticflickr.com/%(server)s/%(id)s_%(secret)s_m.jpg"
+PAGE_TMPL = "http://flickr.com/photos/%(owner)s/%(id)s/in/pool-1876758@N22"
+
 
 class RelatedFlickrJson(BrowserView):
 
+    """Makes two Flickr API calls and writes the number of related photos and URLs
+    for the most viewed related photo from the Pleiades Places group to JSON for
+    use in the Flickr Photos portlet on every Pleiades place page.
+    """
+
     def __call__(self, **kw):
         data = {}
-        # Search photos
+        # Count of related photos
         h = httplib2.Http()
         q = dict(
             method="flickr.photos.search",
@@ -28,41 +37,49 @@ class RelatedFlickrJson(BrowserView):
             format="json",
             nojsoncallback=1 )
         resp, content = h.request(FLICKR_API_ENDPOINT + "?" + urlencode(q), "GET")
+        
         log.debug("Response: %s", resp)
         log.debug("Content: %s", content)
+        
         if resp['status'] == "200":
             total = 0
-            photos = simplejson.loads(content).get("photos")
+            photos = simplejson.loads(content).get('photos')
             if photos:
-                total = int(photos["total"])
+                total = int(photos['total'])
             url = FLICKR_TAGS_BASE + "pleiades:*=%s/" % self.context.getId(),
             data['related'] = dict(total=total, url=url)
         
-        # Get "portrait" from pool
+        # Get portrait photo from group pool
         h = httplib2.Http()
         q = dict(
             method="flickr.groups.pools.getPhotos",
             api_key=FLICKR_API_KEY,
-            group_id="1876758@N22",
+            group_id=PLEIADES_PLACES_ID,
             tags="pleiades:depicts=%s" % self.context.getId(),
+            extras="views",
             format="json",
             nojsoncallback=1 )
         resp, content = h.request(FLICKR_API_ENDPOINT + "?" + urlencode(q), "GET")
+        
         log.debug("Response: %s", resp)
         log.debug("Content: %s", content)
-        if resp['status'] == "200":
+        
+        if resp['status'] == '200':
             total = 0
-            photos = simplejson.loads(content).get("photos")
+            photos = simplejson.loads(content).get('photos')
             if photos:
-                total = int(photos["total"])
+                total = int(photos['total'])
             if total < 1:
-                data["portrait"] = None
+                data['portrait'] = None
             else:
-                photo = photos["photo"][0]
-                img = "http://farm%(farm)s.staticflickr.com/%(server)s/%(id)s_%(secret)s_m.jpg" % photo
-                page = "http://flickr.com/photos/%(owner)s/%(id)s/in/pool-1876758@N22" % photo
-                title = photo["title"] + " by " + photo["ownername"]
-                data["portrait"] = dict(title=title, img=img, page=page)
+                most_viewed = sorted(
+                    photos['photo'], key=lambda p: p['views'], reverse=True )
+                photo = most_viewed[0]
+                img = IMG_TMPL % photo
+                page = PAGE_TMPL % photo
+                title = photo['title'] + " by " + photo['ownername']
+                data['portrait'] = dict(title=title, img=img, page=page)
+        
         self.request.response.setStatus(200)
         self.request.response.setHeader('Content-Type', 'application/json')
         return simplejson.dumps(data)
